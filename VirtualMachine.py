33ba3bc71bc66636bd301153d0136ab71630ed4a
OUMontiel <omarume@gmail.com>
2021-11-24 22:28:31 -0600

    Add virtual machine functionality for arithmetic expressions

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -44,16 +44,101 @@ def storeConstants(constantMemory, memory):
         constantMemory[2][int(address)] = content
     return constantMemory
 
+def getContent(address, virtualMemory):
+    if (address >= 1000 and address <= 3999):
+        return virtualMemory[0][(address // 1000 + 2) % 3][address]
+    elif (address >= 4000 and address <= 6999):
+        return virtualMemory[1][-1][(address // 1000 + 2) % 3][address]
+    elif (address >= 7000 and address <= 9999):
+        return virtualMemory[2][-1][(address // 1000 + 2) % 3][address]
+    elif (address >= 10000 and address <= 12999):
+        return virtualMemory[3][-1][(address // 1000 + 2) % 3][address]
+    elif (address >= 13000 and address <= 15999):
+        return virtualMemory[4][(address // 1000 + 2) % 3][address]
+
+def setContent(value, address, virtualMemory):
+    if (address >= 1000 and address <= 3999):
+        virtualMemory[0][(address // 1000 + 2) % 3][address] = value
+    elif (address >= 4000 and address <= 6999):
+        virtualMemory[1][-1][(address // 1000 + 2) % 3][address] = value
+    elif (address >= 7000 and address <= 9999):
+        virtualMemory[2][-1][(address // 1000 + 2) % 3][address] = value
+    elif (address >= 10000 and address <= 12999):
+        virtualMemory[3][-1][(address // 1000 + 2) % 3][address] = value
+    elif (address >= 13000 and address <= 15999):
+        virtualMemory[4][(address // 1000 + 2) % 3][address] = value
+
+def getType(address):
+    if (address // 1000 % 3 == 1):
+        return 'int'
+    elif (address // 1000 % 3 == 2):
+        return 'float'
+    else:
+        return 'char'
+
+def runAssignment(quadruple, virtualMemory):
+    right_operand = getContent(quadruple[1], virtualMemory)
+    if (right_operand == None):
+        print('ERROR: Variables have not been assigned!')
+        exit()
+    setContent(right_operand, quadruple[3], virtualMemory)
+
+def runExpression(quadruple, virtualMemory):
+    left_type = getType(quadruple[1])
+    right_type = getType(quadruple[2])
+    left_operand = getContent(quadruple[1], virtualMemory)
+    right_operand = getContent(quadruple[2], virtualMemory)
+    if (left_operand == None or right_operand == None):
+        print('ERROR: Variables have not been assigned!')
+        exit()
+    value = calculateExpression(quadruple[0], left_operand, right_operand, left_type, right_type)
+    setContent(value, quadruple[3], virtualMemory)
+
+def calculateExpression(operator, left_operand, right_operand, left_type, right_type):
+    if (operator == '||'):
+        return 1 if left_operand or right_operand else 0
+    elif (operator == '&&'):
+        return 1 if left_operand and right_operand else 0
+    elif (operator == '=='):
+        return 1 if left_operand == right_operand else 0
+    elif (operator == '!='):
+        return 1 if left_operand != right_operand else 0
+    elif (operator == '<'):
+        return 1 if left_operand < right_operand else 0
+    elif (operator == '<='):
+        return 1 if left_operand <= right_operand else 0
+    elif (operator == '>'):
+        return 1 if left_operand > right_operand else 0
+    elif (operator == '>='):
+        return 1 if left_operand >= right_operand else 0
+    elif (operator == '+'):
+        return left_operand + right_operand
+    elif (operator == '-'):
+        return left_operand - right_operand
+    elif (operator == '*'):
+        return left_operand * right_operand
+    elif (operator == '/'):
+        if (left_type == 'int' and right_type == 'int'):
+            return left_operand // right_operand
+        else:
+            return left_operand / right_operand
+    elif (operator == '%'):
+        return left_operand % right_operand
+
 def runProgram(functionDirectory, semanticCube, memory, quadruples):
     globalMemory = generateGlobalMemory()
+    localMemory = [generateLocalMemory()]
+    temporalMemory = [generateTemporalMemory()]
+    temporalPointerMemory = [generateTemporalPointerMemory()]
     constantMemory = generateConstantMemory()
     storeConstants(constantMemory, memory)
+    virtualMemory = [globalMemory, localMemory, temporalMemory, temporalPointerMemory, constantMemory]
 
     quadruplePointer = 0
     while True:
         quadruple = quadruples[quadruplePointer]
         if (quadruple[0] == '='):
-            print(quadruple[0])
+            runAssignment(quadruple, virtualMemory)
             quadruplePointer += 1
         elif (quadruple[0] == 'return'):
             print(quadruple[0])
@@ -93,7 +178,7 @@ def runProgram(functionDirectory, semanticCube, memory, quadruples):
             quadruplePointer += 1
             break
         else:
-            print(quadruple[0])
+            runExpression(quadruple, virtualMemory)
             quadruplePointer += 1
 
 while True:
@@ -112,7 +197,7 @@ while True:
     semanticCube = data['semanticCube']
     memory = data['memory']
     quadruples = data['quadruples']
-    for i in range(len(quadruples)):
-        print(f'{str(i):>4}', ': ', quadruples[i])
+    #for i in range(len(quadruples)):
+    #    print(f'{str(i):>4}', ': ', quadruples[i])
     runProgram(functionDirectory, semanticCube, memory, quadruples)
     
